9bb53a5bb8ecfe9306d725e33b7c83f836607527
acutapugione <acuta.pugione@gmail.com>
2023-12-08 10:48:06 +0200

    acuta at work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from uvicorn import run
 from scrappers import NitterScraper
 
 app = FastAPI()
@@ -10,3 +10,13 @@ async def tweet_search(query: str, since: str = None, until: str = None):
     scraper = NitterScraper()
     result = await scraper.scrape_tweeter_tweets_by_date(query, since, until)
     return {"tweets": result}
+
+
+def main():
+    run("main:app", 
+        reload=True
+        )
+    
+    
+if __name__ == "__main__":
+    main()
