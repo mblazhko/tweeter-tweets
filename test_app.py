ed685044f275a65fbdd92149a29cd780187c8a0f
acutapugione <acuta.pugione@gmail.com>
2023-12-08 15:52:51 +0200

    ,,,

--- test_app.py
+++ test_app.py
@@ -19,8 +19,10 @@ def client_get(params):
 
 def test_read_search():
     with ThreadPoolExecutor(max_workers=10) as pool:
-        results = pool.map(client_get, queries, timeout=10)
+        results = pool.map(client_get, queries, timeout=5)
         for result in results:
+            print(result)
             assert result.status_code == 200
-    
-    
+  
+if __name__ == "__main__":  
+    test_read_search()
